94943d36a1b7e4e063139bee1232f66f71f2c82e
Jonas Dourado <jonas.jonaias@gmail.com>
2017-03-15 19:52:03 -0300

    Use timer functions derived from Qt

--- esp8266_linux.c
+++ esp8266_linux.c
@@ -13,18 +13,19 @@
 #endif
 
 
-uint32_t getCurrentMS (){
-    struct timespec spec;
-
-    clock_gettime(CLOCK_REALTIME, &spec);
-
-    return round(spec.tv_nsec / 1.0e6) + spec.tv_sec * 1000; // Convert nanoseconds to milliseconds
-}
-
-void delayMS(uint32_t ms){
-    uint32_t start = getCurrentMS();
-    while (getCurrentMS() - start < ms);
-}
+//Use timer functions derived from Qt, implemented in IOInterface
+//
+//uint32_t getCurrentMS (){
+//    struct timespec spec;
+//
+//    clock_gettime(CLOCK_REALTIME, &spec);
+//
+//    return round(spec.tv_nsec / 1.0e6) + spec.tv_sec * 1000; // Convert nanoseconds to milliseconds
+//}
+//void delayMS(uint32_t ms){
+//    uint32_t start = getCurrentMS();
+//    while (getCurrentMS() - start < ms);
+//}
 
 
 void espPrintln(int fd, char *buf, int len){
